61817b73e4ff9d41c5efbf1dd0dcff470306ceea
曾彬 <hellozengbin@qq.com>
2023-05-17 14:21:55 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 //后端项目地址 https://github.com/Binaryify/NeteaseCloudMusicApi
-export const BASE_URL = '//116.204.127.62:3000';
+export const BASE_URL = 'https://netease-cloud-music-api-beryl-eight-19.vercel.app';
